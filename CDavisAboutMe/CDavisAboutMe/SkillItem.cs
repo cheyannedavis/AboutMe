507a849dc0aa1223f550c7c9f56eedd8b6104f0a
0775038 <0775038@KELCE-ACIS16-P>
2019-10-30 17:34:45 -0500

    Finished the Skills Page

---
+++ CDavisAboutMe/CDavisAboutMe/SkillItem.cs
@@ -0,0 +1,8 @@
+﻿namespace CDavisAboutMe
+{
+    public class SkillItem
+    {
+        public string Icon { get; set; }
+        public string Name { get; set; }
+    }
+}
--- CDavisAboutMe/CDavisAboutMe/SkillsPage.xaml.cs
+++ CDavisAboutMe/CDavisAboutMe/SkillsPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,32 @@ namespace CDavisAboutMe
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SkillsPage : ContentPage
     {
+        public ObservableCollection<SkillItem> Skills { get; set; }
         public SkillsPage()
         {
             InitializeComponent();
+
+            this.Skills = new ObservableCollection<SkillItem>();
+
+            SkillItem microsoftSkill = new SkillItem();
+            microsoftSkill.Name = "Microsoft Office";
+            microsoftSkill.Icon = "microsoft.png";
+
+            SkillItem sqlSkill = new SkillItem();
+            sqlSkill.Name = "SQL and Database";
+            sqlSkill.Icon = "db.png";
+
+            SkillItem problemSkill = new SkillItem();
+            problemSkill.Name = "Problem Solving";
+            problemSkill.Icon = "mindmap.png";
+
+            Skills.Add(microsoftSkill);
+            Skills.Add(sqlSkill);
+            Skills.Add(problemSkill);
+
+            skillsList.ItemsSource = this.Skills;
+
+
         }
     }
 }
